391d0631ddb8a6f5ca2e6c68cbfb009c5a08d9fd
Salim Ghbabra <sghbabra@hotmail.fr>
2019-03-03 20:51:16 +0100

    Mise en place Jersey ClientBuilder. Marche pas top...

--- tp/src/main/java/fr/orleans/miage/iisi/tp1ws/controllers/IDicoController.java
+++ tp/src/main/java/fr/orleans/miage/iisi/tp1ws/controllers/IDicoController.java
@@ -1,20 +1,24 @@
 package fr.orleans.miage.iisi.tp1ws.controllers;
 
+import exceptions.MaxNbCoupsException;
+import exceptions.MotInexistantException;
+import exceptions.PseudoDejaPrisException;
+import exceptions.PseudoNonConnecteException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public interface IDicoController {
 
-        ResponseEntity connexion(@RequestParam String pseudo);
+        ResponseEntity connexion(@RequestParam String pseudo) throws PseudoDejaPrisException;
 
-        ResponseEntity creerPartie(@PathVariable String pseudo, @RequestParam String dicoName);
+        ResponseEntity creerPartie(@PathVariable String pseudo, @RequestParam String dicoName) throws PseudoNonConnecteException;
 
-        ResponseEntity listeCoupsPrecedents(@PathVariable String pseudo);
+        ResponseEntity listeCoupsPrecedents(@PathVariable String pseudo) throws PseudoNonConnecteException;
 
-        ResponseEntity jouer(@PathVariable String pseudo, @RequestParam String mot);
+        ResponseEntity jouer(@PathVariable String pseudo, @RequestParam String mot) throws MotInexistantException, MaxNbCoupsException, PseudoNonConnecteException;
 
-        ResponseEntity deconnexion(@PathVariable String pseudo);
+        ResponseEntity deconnexion(@PathVariable String pseudo) throws PseudoNonConnecteException;
 
         ResponseEntity listeDicos();
 
--- tp/src/main/java/fr/orleans/miage/iisi/tp1ws/errors/CustomizeExceptionErrors.java
+++ tp/src/main/java/fr/orleans/miage/iisi/tp1ws/errors/CustomizeExceptionErrors.java
@@ -12,36 +12,32 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 public class CustomizeExceptionErrors extends ResponseEntityExceptionHandler {
 
-
     private static final String PASCONNECTE = "Vous n'êtes pas connecté !";
     private static final String PSEUDODEJAPRIS = "Le pseudo est déjà utilisé !";
     private static final String MOTINEXISTANT = "Mot inexistant !";
     private static final String LIMITEMAXCOUPS = "Vous avez atteint la limite maximum de coups autorisés !";
 
-
-
     @ExceptionHandler(PseudoDejaPrisException.class)
     public final ResponseEntity<ErrorsDetails> handlePseudosDejaPris(PseudoDejaPrisException ex, WebRequest request){
         ErrorsDetails errorsDetails = new ErrorsDetails(PSEUDODEJAPRIS, request.getDescription(false));
-        return  ResponseEntity.status(HttpStatus.CONFLICT).body(errorsDetails);
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(errorsDetails);
     }
 
     @ExceptionHandler(PseudoNonConnecteException.class)
     public final ResponseEntity<ErrorsDetails> handlePseudoNonConnecte(PseudoNonConnecteException ex, WebRequest request){
         ErrorsDetails errorsDetails = new ErrorsDetails(PASCONNECTE, request.getDescription(false));
-        return  ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorsDetails);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorsDetails);
     }
-
 
     @ExceptionHandler(MotInexistantException.class)
     public final ResponseEntity<ErrorsDetails> handleMotInexistantException(MotInexistantException ex, WebRequest request){
         ErrorsDetails errorsDetails = new ErrorsDetails(MOTINEXISTANT, request.getDescription(false));
-        return  ResponseEntity.status(HttpStatus.GONE).body(errorsDetails);
+        return ResponseEntity.status(HttpStatus.GONE).body(errorsDetails);
     }
 
     @ExceptionHandler(MaxNbCoupsException.class)
     public final ResponseEntity<ErrorsDetails> handleMaxNbCoupsException(MaxNbCoupsException ex, WebRequest request){
         ErrorsDetails errorsDetails = new ErrorsDetails(LIMITEMAXCOUPS, request.getDescription(false));
-        return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorsDetails);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorsDetails);
     }
 }
